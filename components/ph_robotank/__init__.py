fe42144af80430744f56acfa1afa4d6ea90f8708
Richie Vos <vosrichie@gmail.com>
2024-01-29 21:26:25 -0800

    tweak

--- components/ph_robotank/__init__.py
+++ components/ph_robotank/__init__.py
@@ -17,11 +17,9 @@ PHSensor = ph_sensor_ns.class_('PHSensor', cg.PollingComponent, sensor.Sensor)
 CONFIG_SCHEMA = sensor.sensor_schema(PHSensor, unit_of_measurement='pH', accuracy_decimals=2).extend({
     cv.GenerateID(): cv.declare_id(PHSensor),
     cv.required(CONF_ADDRESS): cv.positive_int,
-
 }).extend(cv.polling_component_schema('60s'))
 
 def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID], config.get(CONF_INTERVAL))
-    cg.add(var.set_address(config[CONF_ADDRESS]))
+    var = cg.new_Pvariable(config[CONF_ID], config.get(CONF_INTERVAL), config[CONF_ADDRESS])
     yield cg.register_component(var, config)
     yield sensor.register_sensor(var, config)
