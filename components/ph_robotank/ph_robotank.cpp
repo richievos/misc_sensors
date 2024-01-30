4ee73345c00ff25d90ec7044776672508754f09e
Richie Vos <vosrichie@gmail.com>
2024-01-29 22:39:07 -0800

    tweak

--- components/ph_robotank/ph_robotank.cpp
+++ components/ph_robotank/ph_robotank.cpp
@@ -1,4 +1,3 @@
-#pragma once
 #include <Wire.h>
 #include "ph_robotank.h"
 
@@ -7,6 +6,31 @@
 
 namespace esphome {
 namespace ph_robotank {
+
+// TODO: figure out how to avoid copy-pasting this file
+// read the current ph directly from the board
+float esphome_readPHSignal_RoboTankPHBoard(const uint8_t i2cAddress) {
+    ::Wire.beginTransmission(i2cAddress);  // start transmission
+    ::Wire.write("R");                     // ask for pH
+    ::Wire.write(0);                       // send closing byte
+    ::Wire.endTransmission();              // end transmission
+
+    uint8_t count = 0;  // count imcoming bytes
+    uint8_t byteCountToRead = 8;
+    String i = "";                                  // create string of incoming data
+    ::Wire.requestFrom(i2cAddress, byteCountToRead);  // request 8 bytes from pH circuit
+    while (::Wire.available())                        // read the 8 incoming bytes
+    {
+        char c = Wire.read();  // receive a byte as character
+        if (count > 0) {
+            i = i + c;
+        }         // ignore first btye and combine remaining into a string
+        count++;  // count byte
+    }
+    float pH = i.toFloat();  // convert pH string to float
+    return pH;               // return pH
+}
+
 
 void PHSensor::setup() {
     // This will be called by ESPHome upon startup, initialize the sensor
