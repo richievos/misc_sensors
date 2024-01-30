a90346504318f26bfcca3042d673c550f2faa9d3
Richie Vos <vosrichie@gmail.com>
2024-01-29 20:52:54 -0800

    Fixed init

--- components/ph_robotank/__init__.py
+++ components/ph_robotank/__init__.py
@@ -1,42 +1,19 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import esp32_ble_tracker
-from esphome.core import coroutine
-from esphome.const import (
-    CONF_ID,
-)
+from esphome.components import sensor
+from esphome.const import CONF_ID
 
-CODEOWNERS = ["@myhomeiot"]
-DEPENDENCIES = ["esp32_ble_tracker"]
+DEPENDENCIES = []
+MULTI_CONF = True
 
-myhomeiot_ble_host_ns = cg.esphome_ns.namespace("myhomeiot_ble_host")
-MyHomeIOT_BLEHost = myhomeiot_ble_host_ns.class_(
-    "MyHomeIOT_BLEHost", cg.Component, esp32_ble_tracker.ESPBTClient
-)
+ph_sensor_ns = cg.esphome_ns.namespace('ph_sensor')
+PHSensor = ph_sensor_ns.class_('PHSensor', cg.PollingComponent, sensor.Sensor)
 
-CONFIG_SCHEMA = (
-    cv.Schema(
-        {
-            cv.GenerateID(): cv.declare_id(MyHomeIOT_BLEHost),
-        }
-    )
-    .extend(cv.COMPONENT_SCHEMA)
-    .extend(esp32_ble_tracker.ESP_BLE_DEVICE_SCHEMA)
-)
+CONFIG_SCHEMA = sensor.sensor_schema(PHSensor, cg.uint32_t, unit_of_measurement='pH', accuracy_decimals=2).extend({
+    cv.GenerateID(): cv.declare_id(PHSensor),
+}).extend(cv.polling_component_schema('60s'))
 
-CONF_BLE_HOST_ID = "ble_host_id"
-
-BLE_CLIENT_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(CONF_BLE_HOST_ID): cv.use_id(MyHomeIOT_BLEHost),
-    }
-)
-
-async def register_ble_client(var, config):
-    parent = await cg.get_variable(config[CONF_BLE_HOST_ID])
-    cg.add(parent.register_ble_client(var))
-
-async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    await esp32_ble_tracker.register_client(var, config)
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID], config.get(CONF_INTERVAL))
+    yield cg.register_component(var, config)
+    yield sensor.register_sensor(var, config)
