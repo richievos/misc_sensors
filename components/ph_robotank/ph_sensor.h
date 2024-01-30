cdb76250fd5876aacdd87b82b56fde6952c9bf53
Richie Vos <vosrichie@gmail.com>
2024-01-29 21:03:28 -0800

    tweak

--- components/ph_robotank/ph_sensor.h
+++ components/ph_robotank/ph_sensor.h
@@ -1,24 +1,23 @@
 #pragma once
 
-#include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
+#include "esphome/core/component.h"
 // #include "esphome/components/i2c/i2c.h"
 
 #include <Wire.h>
+
 #include <memory>
 
 class PHSensor : public PollingComponent {
- public:
+   public:
+    std::unique_ptr<Sensor> _ph_sensor;
+    uint8_t _i2cAddress;
 
-  std::unique_ptr<Sensor> _ph_sensor;
-  uint8_t _i2cAddress;
+    // constructor
+    PHSensor(uint32_t update_interval) : PollingComponent(update_interval), address_(address), _ph_sensor(std::make_unique<Sensor>()) {}
 
-  // constructor
-  PHSensor(uint8_t address, uint32_t update_interval) : PollingComponent(update_interval), address_(address), _ph_sensor(std::make_unique<Sensor>()) {}
+    void setup() override;
+    void update() override;
 
-  void setup() override;
-  void update() override;
-
- private:
-  float convert_to_ph(byte *data);
+    void set_address(uint8_t address) { _i2cAddress = address; }
 };
