ae119696a739ce106668b0a56dca0e812a0c37b7
Richie Vos <vosrichie@gmail.com>
2024-01-29 22:07:15 -0800

    tweak

--- components/ph_robotank/sensor.py
+++ components/ph_robotank/sensor.py
@@ -37,6 +37,7 @@ CONFIG_SCHEMA = sensor.sensor_schema(PHSensor, unit_of_measurement='pH', accurac
 #     yield sensor.register_sensor(var, config)
 
 async def to_code(config):
+    pass
     # var = await sensor.new_sensor(config)
     # # , config.get(CONF_INTERVAL), config[CONF_ADDRESS]
     # await cg.register_component(var, config)
