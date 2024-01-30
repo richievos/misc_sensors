3d8c47fb7360be40cb7f3dee6e365f0176dcf54a
Richie Vos <vosrichie@gmail.com>
2024-01-29 21:38:51 -0800

    tweak

--- components/ph_robotank/sensor.py
+++ components/ph_robotank/sensor.py
@@ -14,8 +14,21 @@ MULTI_CONF = True
 ph_sensor_ns = cg.esphome_ns.namespace('ph_robotank')
 PHSensor = ph_sensor_ns.class_('PHSensor', cg.PollingComponent, sensor.Sensor)
 
+# sensor.sensor_schema(
+#     ADCMPXSensor,
+#     unit_of_measurement=UNIT_VOLT,
+#     icon=ICON_EMPTY,
+#     accuracy_decimals=2,
+#     device_class=DEVICE_CLASS_VOLTAGE
+# ).extend(
+#     {
+#         cv.Required(CONF_SENSOR): cv.use_id(voltage_sampler.VoltageSampler),
+#         cv.Required(CONF_PIN): pins.gpio_output_pin_schema
+#     }
+# ).extend(cv.polling_component_schema("60s"))
+
+
 CONFIG_SCHEMA = sensor.sensor_schema(PHSensor, unit_of_measurement='pH', accuracy_decimals=2).extend({
-    cv.GenerateID(): cv.declare_id(PHSensor),
     cv.Required(CONF_ADDRESS): cv.positive_int,
 }).extend(cv.polling_component_schema('60s'))
 
