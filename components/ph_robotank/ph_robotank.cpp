03f72328493233490355f1f6316cf9d91ed655ce
Richie Vos <vosrichie@gmail.com>
2024-01-29 21:27:16 -0800

    tweak

---
+++ components/ph_robotank/ph_robotank.cpp
@@ -0,0 +1,24 @@
+// #include "esphome.h"
+// #include <Wire.h>
+#include "ph_robotank.h"
+
+#include "../../lib/robotank/ph-robotank-sensor.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace ph_robotank {
+
+void PHSensor::setup() {
+    // This will be called by ESPHome upon startup, initialize the sensor
+    setupPH_RoboTankPHBoard();
+}
+
+void PHSensor::update() {
+    const auto ph = readPHSignal_RoboTankPHBoard(_i2cAddress);
+
+    ph_sensor->publish_state(ph);
+}
+
+}  // namespace ph_robotank
+
+}  // namespace esphome
